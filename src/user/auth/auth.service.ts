9f01c73bdd180d264142075705c56a0a2d9c0178
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-10-30 23:57:54 +0530

    feat: auth

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,18 +1,29 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, HttpException, Injectable } from "@nestjs/common";
 import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { UserType } from '@prisma/client';
 
-interface SignupParams{
+interface SignupParams {
   email: string;
   password: string;
   name: string;
   phone: string;
 }
 
+interface SigninParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
+
+
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService){}
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async signup({ email }: SignupParams) {
+  async signup({ email, password, name, phone }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: {
         email,
@@ -22,5 +33,67 @@ export class AuthService {
     if (userExists) {
       throw new ConflictException();
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    const token = jwt.sign(
+      {
+        name,
+        id: user.id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: 9000000,
+      },
+    );
+
+    return token;
+  }
+
+  async signin({ email, password }: SigninParams) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Invalid Credentials', 400);
+    }
+
+    const hashedPassword = user.password;
+
+    const isValidPassword = await bcrypt.compare(password, hashedPassword);
+
+    if (!isValidPassword) {
+      throw new HttpException('Invalid Credentials', 400);
+    }
+
+    const token = this.generateJWT(user.name, user.id);
+
+    return token;
+  }
+
+  generateJWT(name: string, id: number){
+    return jwt.sign(
+      {
+        name,
+        id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: 9000000,
+      },
+    );
   }
 }
--- src/user/dtos/auth.dto.ts
+++ src/user/dtos/auth.dto.ts
@@ -24,3 +24,11 @@ export class SignupDto {
   @MinLength(6)
   password: string;
 }
+
+export class SigninDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
