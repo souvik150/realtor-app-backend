6ff7c5f033b5ff60ba4cf77117dc65d945e637d6
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-10-30 16:53:29 +0530

    fix: email validation

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import {PrismaClient} from "prisma/prisma-client";
-import {OnModuleDestroy, OnModuleInit} from "@nestjs/common";
+import { PrismaClient } from 'prisma/prisma-client';
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  // async OnModuleInit() {
+  //   await this.$connect();
+  // }
+  //
+  // async OnModuleDestroy() {
+  //   await this.$disconnect();
+  // }
 
+  onModuleDestroy(): any {
+    this.$connect();
+  }
+
+  onModuleInit(): any {
+    this.$disconnect();
+  }
 }
